55be72fe13655db9e4a5456b73a83d76019cbf4f
pan <757273952@qq.com>
2017-12-10 02:12:40 +0000

    Finish user signup

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,10 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
     public function create(){
-        return view('users/create');
+        return view('users.create');
+    }
+    public function show(User $user){
+        return view('users.show',compact('user'));
+    }
+    public function store(Request $request){
+        $this->validate($request,[
+            'name' => 'required|max:50',
+            'email' => 'email',
+            'password' => 'confirmed'
+        ]);
+        $user=User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        session()->flash('success','注册成功');
+        return redirect()->route('users.show',[$user]);
     }
 }
